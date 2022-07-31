bf2e63a70c1bb946de28e667accb536247056d7b
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-07-31 18:22:25 -0400

    Update spawnshot.lua

--- spawnshot.lua
+++ spawnshot.lua
@@ -1,1 +1,3 @@
-game.Workspace.Prison_ITEMS.giver["Remington 870"].ITEMPICKUP.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+local A_1 = game:GetService("Workspace")["Prison_ITEMS"].giver["Remington 870"].ITEMPICKUP
+local Event = game:GetService("Workspace").Remote.ItemHandler
+Event:InvokeServer(A_1)
