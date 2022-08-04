ce0829cd8ab1f0a5ecb76fb1e99ae64ca6297e75
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-03 23:58:48 -0400

    Create arrestall.lua

---
+++ arrestall.lua
@@ -0,0 +1,14 @@
+wait(0.1)
+	Player = game.Players.LocalPlayer
+	Pcf = Player.Character.HumanoidRootPart.CFrame
+	for i,v in pairs(game.Teams.Criminals:GetPlayers()) do
+	if v.Name ~= Player.Name then
+	local i = 10
+    repeat
+    wait()
+    i = i-1
+    game.Workspace.Remote.arrest:InvokeServer(v.Character.HumanoidRootPart)
+    Player.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 1)
+    until i == 0
+end
+end
