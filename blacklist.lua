3d704a5a5089f340406167f5090f289ddce5f963
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-03 23:30:22 -0400

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,5 +1,5 @@
 local arr = {3721753350}
 local userid = game:GetService('Players').LocalPlayer.UserId
-	if userid == #arr then
+	if userid == (arr[#arr]) then
 		game.Players.LocalPlayer:Kick("L bozo")
 	end
