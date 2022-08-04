efb8a9cf32401d8d70feba23dc216253193d4f50
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-03 20:42:45 -0400

    Update criminal.lua

--- criminal.lua
+++ criminal.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-920.291, 106.307, 2139.63)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-875.396, 93.7928, 2063.9)
