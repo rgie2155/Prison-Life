6e28b9d20e74d1c83e1628cdcdbf57978d768a1e
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-03 23:48:44 -0400

    Update criminalteam.lua

--- criminalteam.lua
+++ criminalteam.lua
@@ -1,1 +1,10 @@
-
+LCS = game.Workspace["Criminals Spawn"].SpawnLocation
+ 
+LCS.CanCollide = false
+LCS.Size = Vector3.new(51.05, 24.12, 54.76)
+LCS.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+LCS.Transparency = 1
+wait(0.5)
+LCS.CFrame = CFrame.new(-920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+LCS.Size = Vector3.new(6, 0.2, 6)
+LCS.Transparency = 0
