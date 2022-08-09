5e05100d9a8bc254b6bc874ea73a928da81455d0
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-08 23:46:02 -0400

    Update ScreenGui.lua

--- ScreenGui.lua
+++ ScreenGui.lua
@@ -329,8 +329,11 @@ tpplayer.TextSize = 14.000
 tpplayer.TextWrapped = true
 tpplayer.MouseButton1Down:connect(function()
 	local target = "tptarget.Text"
+	print("Target successfully found!")
 	local localplayer = game.Players[target].Character.HumanoidRootPart.CFrame
+	print("Found the targets position")
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = localplayer
+	print("Teleport successful")
 end)
 
 tptarget.Name = "tptarget"
