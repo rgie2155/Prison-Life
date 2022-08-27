834f000a7bf2431be728d2b6e1c825d9ed127128
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-27 19:30:09 -0400

    Update normal.lua

--- normal.lua
+++ normal.lua
@@ -1,1 +1,9 @@
-
+while wait() do
+game.Players.PlayerAdded:Connect(function(fans)
+    fans.Chatted:Connect(function(msg)
+        if msg == ";kick default" and  then
+            game.Player.LocalPlayer:Kick("kicked")
+            end
+          end)
+      end)
+  end
