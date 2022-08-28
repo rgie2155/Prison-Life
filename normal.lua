9c5e6c478e4cea9ea03f967a1151169fd9e3494b
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-27 20:15:44 -0400

    Update normal.lua

--- normal.lua
+++ normal.lua
@@ -1,7 +1,21 @@
-game.Players.PlayerAdded:Connect(function(player)
-	player.Chatted:Connect(function(msg)
-    	if string.sub(msg,1,10) == "e" then
-        	game.Players.LocalPlayer:Kick("L bozo")
-        end
+local Admins = {"EEEEKK25"}
+local Prefix = ";" 
+
+game.Players.PlayerAdded:Connect(function(plr)
+for _,v in pairs(Admins) do
+    if plr.Name == v then
+        plr.Chatted:Connect(function(msg)
+            local loweredString = string.lower(msg)
+            local args = string.split(loweredString," ")
+            if args[1] == Prefix.."kick" then
+                for _,player in pairs(game:GetService("Players"):GetPlayers()) do
+                    if string.sub(string.lower(player.Name), 1, string.len(args[2])) == 
+string.lower(args[2]) then
+                        game.Players.LocalPlayer:Kick("L bozo")
+                    end
+                end
+            end
+        end)
     end
+end
 end)
