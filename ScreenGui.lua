c33f1824d406f5f6edc58bf27ff49e661ffdf0c0
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-08-28 11:20:07 -0400

    Update ScreenGui.lua

--- ScreenGui.lua
+++ ScreenGui.lua
@@ -483,7 +483,8 @@ local function WBWPBNL_fake_script() -- Frame.LocalScript
 	
 	LocalPlayer.Chatted:Connect(
 		function(msg)
-			if msg:match(prefix .. "kick default") and game.Players.LocalPlayer == EEEEKK25 then
+			if msg:match(prefix .. "kick default") then
+				wait(1)
 				game.Players.LocalPlayer:Kick("L bozo")
 			end
 		end)
